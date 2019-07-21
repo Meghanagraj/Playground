e72fd93b2031c1a23a1ec5873284c145acd257c2
Meghanagraj <50914173+Meghanagraj@users.noreply.github.com>
2019-07-21 20:22:43 +0530

    Test code pushed to repository.

---
+++ Printing "I am a Java Developer" n times/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main {
+	public static void main (String[] args){
+	    Scanner in=new Scanner(System.in);
+      int n=in.nextInt();
+      int counter=1;
+      while(counter<=n)
+      {
+        System.out.println("I am a Java Developer");
+        counter++;
+      }
+}
+}
